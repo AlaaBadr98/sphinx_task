ca634a2b29057e3930eef5beb63faac297ad7250
AlaaOsama <alaaosama2121998@gmail.com>
2023-04-03 01:05:15 +0200

    commit message

--- js/js.js
+++ js/js.js
@@ -39,42 +39,94 @@ function removeHelpBtn() {
 //  the solutions of questions 
 var sol='' , questionNum='';
 function saveSol(val,ans){
-console.log(val);
- document.getElementById(val).style.color ="#fff";
- document.getElementById(val).style.backgroundColor ="#0fa0c5";
- sol=ans;
- questionNum=val
-
-};
-function putSol(ans){
-    if(ans===sol){
-       document.getElementById("correct").play();
-       document.getElementById(questionNum).style.color ="#000";
-       document.getElementById(questionNum).style.backgroundColor ="#fff";
-       showAns(questionNum);
-       sol='' ; questionNum='';
-       
-
-
+    if(val===1){
+        document.getElementById('sol_1').style.color ="#fff";
+        document.getElementById('sol_1').style.backgroundColor ="#0fa0c5";
+        document.getElementById('sol_2').style.color ="#0fa0c5";
+        document.getElementById('sol_2').style.backgroundColor ="#fff";
+        questionNum=1;
     }else{
-        document.getElementById("incorrect").play();
+        document.getElementById('sol_2').style.color ="#fff";
+        document.getElementById('sol_2').style.backgroundColor ="#0fa0c5";
+        document.getElementById('sol_1').style.color ="#0fa0c5";
+        document.getElementById('sol_1').style.backgroundColor ="#fff";
+        questionNum=2;
     }
-     
-    };
-
-    function showAns(questionNum){
-        console.log(questionNum);
-     if(questionNum=='sol_1'){
-        console.log(questionNum);
+ sol=ans;
+};
+function putSol(num,ans){
+    // right answer 
+    if(ans===sol){ 
+        document.getElementById("correct").play();
+       
+       
+       if(num==1){
         document.getElementById('q_1_sol').style.display='';
+        document.getElementById('ans-1').innerHTML=sol;
+        document.getElementById("img-1").src="media/tikMark-small.png";
      }
-     else if (questionNum=='sol_2'){
+     else if (num==2){
         document.getElementById('q_2_sol').style.display='';
+        document.getElementById('ans-2').innerHTML=sol;
+        document.getElementById("img-2").src="media/tikMark-small.png";
      }
      else {
         document.getElementById('q_3_sol').style.display='';
-        console.log("qjjjjjj");
+        document.getElementById('ans-3').innerHTML=sol;
+        document.getElementById("img-3").src="media/tikMark-small.png";
      }
+
+    //  wrong answer 
+    }else{
+        document.getElementById("incorrect").play();
+        if(num==1){
+            document.getElementById('ans-1').innerHTML=sol;
+            document.getElementById("img-1").src="media/Wrong-PNG-Image.png";
+            setTimeout(function(){
+                document.getElementById('q_1_sol').style.display='none';
+               
+            }, 1000);
+            document.getElementById('q_1_sol').style.display='';
+            num=0;
+            
+         }
+         else if (num==2){
+            document.getElementById('ans-2').innerHTML=sol;
+            document.getElementById("img-2").src="media/Wrong-PNG-Image.png";
+            setTimeout(function(){
+                document.getElementById('q_2_sol').style.display='none';
+               
+            }, 1000);
+            document.getElementById('q_2_sol').style.display='';
+         }
+         else {
+            document.getElementById('ans-3').innerHTML=sol;
+            document.getElementById("img-3").src="media/Wrong-PNG-Image.png";
+            setTimeout(function(){
+                document.getElementById('q_3_sol').style.display='none';
+               
+            }, 1000);
+            document.getElementById('q_3_sol').style.display='';
+         }
     }
+     
+    };
+    //   show all anwsers 
+    function showAllAnswar(){
+        document.getElementById('q_1_sol').style.display='';
+        document.getElementById('q_2_sol').style.display='';
+        document.getElementById('q_3_sol').style.display='';
+
+    }
+//  restart the question 
+    function restart(){
+        document.getElementById('q_1_sol').style.display='none';
+        document.getElementById('q_2_sol').style.display='none';
+        document.getElementById('q_3_sol').style.display='none';
+
+
+    }
+
+    
  
 
